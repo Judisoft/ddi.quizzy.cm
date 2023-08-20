7e7bef286e7ac75c0fdd84e36babbbe3d9fb39e4
Judisoft <kumjude09@gmail.com>
2023-08-20 01:45:51 +0100

    ddi commit reverted

--- resources/views/blank.blade.php
+++
@@ -1,8 +0,0 @@
-@include('layouts.dashboard.main')
-<div class="row">
-    <div class="col-lg-12">
-        <div class="panel-content">
-            content here
-        </div>
-    </div>
-</div>
--- resources/views/vendor/jetstream/components/label.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['value'])
-
-<label {{ $attributes->merge(['class' => 'block py-2 font-bold text-md text-gray-700']) }}>
-    {{ $value ?? $slot }}
-</label>
